af11e9859f74d63a2972f5ab270464ff92eb1200
DutchBeastman <Fabianverk@hotmail.com>
2014-11-11 15:15:24 +0100

    Added Level and player script and Camera Scripts

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Vector2 playerVelocity;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		PlayerMovement();
+	}
+
+	void PlayerMovement()
+	{
+
+		playerVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"));
+		rigidbody2D.AddRelativeForce(playerVelocity * 100);
+	}
+}
