b7fba1e5f2dff992eb7fc02e6ec176fccd0787c9
DutchBeastman <Fabianverk@hotmail.com>
2014-11-12 00:10:30 +0100

    Making cameraFollow

--- Assets/Resources/Scripts/CameraFollow.cs
+++ Assets/Resources/Scripts/CameraFollow.cs
@@ -6,30 +6,32 @@ public class CameraFollow : MonoBehaviour {
 	public GameObject cameraTarget; // object to look at or follow
 	public GameObject player; // player object for moving
 	
-	public float smoothTime = 0.1f; // time for dampen
+	public float smoothTime = 1f; // time for dampen
 	public bool cameraFollowX = true; // camera follows on horizontal
 	public bool cameraFollowY = true; // camera follows on vertical
 	public bool cameraFollowHeight = true; // camera follow CameraTarget object height
 	public float cameraHeight = 2.5f; // height of camera adjustable
 	public Vector2 velocity; // speed of camera movement
 	
-	private Transform thisTransform; // camera Transform
+	private Transform cameraPosition; // camera Transform
 	
 	// Use this for initialization
 	void Start () {
-		thisTransform = transform;
+		cameraPosition = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (cameraFollowX){
-			thisTransform.position.x = Mathf.SmoothDamp (thisTransform.position.x, cameraTarget.transform.position.x, ref velocity.x, smoothTime); // Here i get the error
+			float tempX = Mathf.SmoothDamp (cameraPosition.position.x, cameraTarget.transform.position.x, ref velocity.x, smoothTime);
+			//Doe cameraPosition.position.x in een variable.
+			cameraPosition.position.x = new Vector3 (tempX , cameraPosition.position.y, cameraPosition.position.z);
 		}
 		if (cameraFollowY) {
-			// to do    
+			cameraPosition.position.y = new Vector3 (cameraPosition.position.x,Mathf.SmoothDamp (cameraPosition.position.y, cameraTarget.transform.position.y, ref velocity.y, smoothTime), cameraPosition.position.z);
 		}
 		if (!cameraFollowX & cameraFollowHeight) {
-			// to do
+			//To Do
 		}
 	}
 }
