94ef04e35e8ccbf0a083981945d4995faaeb2bfa
DutchBeastman <Fabianverk@hotmail.com>
2014-11-14 00:48:13 +0100

    Multiplayer Update (not Complete)
    Mulitplayer is not complete yet

---
+++ Assets/Resources/Scripts/NetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "Fabian'sRunnerGameName";
+	private const string gameName = "Fabian'sRunnerGame";
+	private HostData[] hostList;
+	
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName, "A MP runner");
+		//MasterServer.ipAddress = "127.0.0.1";
+	}
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+		Debug.Log("Server Initializied");
+	}
+
+	
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		SpawnPlayer();
+		Debug.Log("Server Joined");
+	}
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(Resources.Load("Prefabs/Player"), new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+	}
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+}
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -2,23 +2,24 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	public Vector2 playerVelocity;
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
+	public float smooth = 1000f;
+	private Vector2 playerVector;
+	private float tempVel;
+
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
 		PlayerMovement();
 	}
 
 	void PlayerMovement()
 	{
+		rigidbody2D.rotation = 0;
+		playerVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"));
+		rigidbody2D.AddRelativeForce(playerVector * 1000 *  Time.deltaTime);
+		if(Input.anyKey == false){
+			rigidbody2D.velocity = new Vector2(Mathf.SmoothDamp(rigidbody2D.velocity.x,0, ref tempVel, smooth),Mathf.SmoothDamp(rigidbody2D.velocity.y,-1, ref tempVel, smooth));
 
-		playerVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"));
-		rigidbody2D.AddRelativeForce(playerVelocity * 100);
+		}
 	}
 }
