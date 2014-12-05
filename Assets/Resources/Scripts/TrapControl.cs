3953714c1ce1055bf5ed5ae16e54aeb752f583fb
kerim birlik <ninja_sky1@hotmail.com>
2014-12-05 06:58:53 +0100

    TrapSelector
    wip

--- Assets/Resources/Scripts/TrapControl.cs
+++ Assets/Resources/Scripts/TrapControl.cs
@@ -7,46 +7,69 @@ public class TrapControl : MonoBehaviour {
 	GameObject CurrentTrap;
 	bool    MovingTrap;
 	public Camera cam;
+	int activeTrap;
 	// Use this for initialization
 	void Start () {
-	
+
+		activeTrap = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(MovingTrap);
+		if(Input.GetKeyDown(KeyCode.F1)){
+			activeTrap = 0;
+		}
+		if(Input.GetKeyDown(KeyCode.F2)){
+			activeTrap = 1;
+			Debug.Log ("trap2");
+		}
+		if(Input.GetKeyDown(KeyCode.F3)){
+			activeTrap = 2;
+			Debug.Log ("trap3");
+		}
 		Ray ray = cam.ScreenPointToRay (Input.mousePosition);
 
 		if(Input.GetMouseButtonDown(0))
 		{
 
 			MovingTrap = true;
-			Debug.Log(Input.mousePosition);
+
+
+
+//			if(activeTrap == 0){
 			GameObject ShootingTrap = Instantiate(Resources.Load("Prefabs/ShootingTrap"), new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z), Quaternion.identity) as GameObject;
 			CurrentTrap = ShootingTrap;
+
+		        if(activeTrap == 1){
+				GameObject SmackTrap = Instantiate(Resources.Load("Prefabs/ShootingTrap"), new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z), Quaternion.identity) as GameObject;
+				CurrentTrap = SmackTrap;
+			}
+			else if(activeTrap == 2){
+				GameObject SpikeTrap = Instantiate(Resources.Load("Prefabs/ShootingTrap"), new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z), Quaternion.identity) as GameObject;
+				CurrentTrap = SpikeTrap;
+			}
 			if(MovingTrap == true){
 				shootingTrapPosition = ray.origin;
 				shootingTrapPosition.z = ray.origin.z + 9.7f;
 				ShootingTrap.transform.localPosition = shootingTrapPosition;
-			
 				Debug.Log(ray.origin);
 			}
 
 
 
 		}
-		if(Input.GetMouseButtonUp(0)){
-			MovingTrap = false;
-
-		}	                 
-
-		if(MovingTrap == true){
-
-			shootingTrapPosition = ray.origin;
-			shootingTrapPosition.z = ray.origin.z + 9.7f;
-			CurrentTrap.transform.localPosition= shootingTrapPosition;
-			
-			Debug.Log(ray.origin);
-	}
+//		if(Input.GetMouseButtonUp(0)){
+//			MovingTrap = false;
+//
+//		}	                 
+//
+//		if(MovingTrap == true){
+//
+//			shootingTrapPosition = ray.origin;
+//			shootingTrapPosition.z = ray.origin.z + 9.7f;
+//			CurrentTrap.transform.localPosition= shootingTrapPosition;
+//			
+//			Debug.Log(ray.origin);
+//	}
 	}
 }
